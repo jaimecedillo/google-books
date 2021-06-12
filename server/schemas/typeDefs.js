d95088611f4f59efa3b3183b0a52db3126c571c8
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-12 10:21:06 -0600

    set up query and mutattion types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,9 +4,35 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 type Query {
-    helloWorld: String
+    me: User
   }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+type Book {
+    bookId: String
+    authors: Strings
+    description: String
+    title: String
+    image: String
+    link: String
+}
 
+type Auth {
+    token: ID!
+    user: User
+}
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput):User
+    removeBook(bookId: String!):User
+  }
 `;
 
 // export the typeDefs
