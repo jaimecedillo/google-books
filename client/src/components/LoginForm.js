8bbfb804c00ab7cd44f7d332711b674abd57b838
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-13 11:09:32 -0600

    fixed login functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -32,7 +32,7 @@ const LoginForm = () => {
 
       });
 
-      Auth.login(data.loginUser.token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       console.log(loginUser);
